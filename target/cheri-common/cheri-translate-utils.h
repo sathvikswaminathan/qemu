a7a60be54f2fd9a0669cf75420213cef32049fc1
sathvikswaminathan <sathvikswaminathan@gmail.com>
2023-05-23 00:51:36 +0530

    debugged error

--- target/cheri-common/cheri-translate-utils.h
+++ target/cheri-common/cheri-translate-utils.h
@@ -453,13 +453,15 @@ static inline void gen_special_interposed_st_i32(
 static inline void generate_cap_ustore_check_imm(TCGv_cap_checked_ptr
 resultaddr, uint32_t capreg_1, uint32_t capreg_2, MemOp op)
 {
-TCGv_i32 tcs1 = tcg_const_i32(capreg_1);
-TCGv_i32 tcs2 = tcg_const_i32(capreg_2);
-TCGv_i32 tsize = tcg_const_i32(memop_size(op));
-gen_helper_cap_ustore_check(resultaddr, cpu_env, tcs1, tcs2, tsize);
-tcg_temp_free_i32(tsize);
-tcg_temp_free_i32(tcs1);
-tcg_temp_free_i32(tcs2);
+    TCGv_i32 tcs1 = tcg_const_i32(capreg_1);
+    TCGv_i32 tcs2 = tcg_const_i32(capreg_2);
+    TCGv_i32 tsize = tcg_const_i32(memop_size(op));
+    
+    gen_helper_cap_ustore_check(cpu_env, tcs1, resultaddr, tcs2, tsize);
+
+    tcg_temp_free_i32(tsize);
+    tcg_temp_free_i32(tcs1);
+    tcg_temp_free_i32(tcs2);
 }
 
 
